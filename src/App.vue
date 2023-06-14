793bdc87df344c90e1101b2b272e8e9ac36cd30d
CatalanoLorenzo <lorenzocatalano1995@outlook.it>
2023-06-14 16:14:23 +0200

    add pagination

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,9 @@ export default {
       projectsPath: 'api/projects',
       loading: true,
       projects: [],
+      links: '',
+      
+
     }
   },
   methods: {
@@ -17,30 +20,32 @@ export default {
         .get(url)
         .then(response => {
           console.log(response);
-          this.projects = response.data.projects.data
+          this.projects = response.data.projects;
+      
         })
         .catch(error => {
           console.error(error.message);
         });
-    }
+    },
   },
-  mounted() {
+    mounted() {
     this.getProjects('http://127.0.0.1:8000/api/projects')
   },
+  currentPage(url){
+
+  },
 }
+
 </script>
 
 
 <template>
-  <header>
-
-  </header>
   <main>
     <div class="container">
       <div class="row">
         <div class="col">
-          <div class="card" v-for="project in projects">
-            <img class="card-img-top" :src="this.baseApi +'storage/'+ project.cover" alt="Title">
+          <div class="card" v-for="project in projects.data">
+            <img class="card-img-top" :src="this.baseApi + 'storage/' + project.cover" alt="Title">
             <div class="card-body">
               <h4 class="card-title">{{ project.title }}</h4>
               <p class="card-text">
@@ -52,19 +57,28 @@ export default {
               <div class="container">
                 <div class="row">
                   <div class="col">
-                    <span class="badge bg-primary" v-for="singletype in project.type">{{singletype.name}}</span>
+                    <span class="badge bg-primary">{{ project.type.name }}</span>
                   </div>
                 </div>
               </div>
             </div>
+
+          </div>
+          <div>
+            <nav aria-label="Page navigation example">
+              <ul class="pagination">
+                <li class="page-item" v-for="link in projects.links" :class="!link.url ? 'disabled' : ''||link.active ? 'actived' : ''">
+                  <a @click="getProjects(link.url)"  class="page-link" v-if="link.label === 'Next &raquo;'">Next</a>
+                  <a @click="getProjects(link.url)"  class="page-link" v-else-if="link.label === '&laquo; Previous'">Previous</a>
+                  <a @click="getProjects(link.url)"  class="page-link" v-else >{{link.label}}</a>
+                </li>
+              </ul>
+            </nav>
           </div>
         </div>
       </div>
     </div>
   </main>
-  <footer>
-
-  </footer>
 </template>
 
 <style lang="scss"></style>
